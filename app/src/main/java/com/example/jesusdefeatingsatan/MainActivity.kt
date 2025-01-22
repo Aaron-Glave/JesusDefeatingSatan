765fe84b662a7c7b6360cb053bc07f298417dfbd
Aaron Glave <aaronglavenowbelivesingod@gmail.com>
2025-01-21 19:44:09 -0600

    Minor update
    The song now properly pauses when the app isn't open.

--- app/src/main/java/com/example/jesusdefeatingsatan/MainActivity.kt
+++ app/src/main/java/com/example/jesusdefeatingsatan/MainActivity.kt
@@ -2,34 +2,33 @@ package com.example.jesusdefeatingsatan
 
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.os.Handler
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlinx.coroutines.delay
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import java.util.Timer
 import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
-    var singer: MediaPlayer? = null
+    val listener = AppLifecycleListener()
     override fun onCreate(savedInstanceState: Bundle?) {
+        listener.installMedia(MediaPlayer.create(baseContext, R.raw.i_wanna_follow_jesus))
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        singer = MediaPlayer.create(this, R.raw.i_wanna_follow_jesus)
-        if (singer != null) {
+        /*if (singer != null) {
             singer!!.start()
+        }*/
+
+        /*singer.setOnCompletionListener {
+            //finish()
         }
-        if (singer != null) {
-            singer!!.setOnCompletionListener {
-                finish()
-            }
-        }
+         */
         setContentView(R.layout.activity_main)
         val myMessage = findViewById<TextView>(R.id.aarons_message)
         val button = findViewById<Button>(R.id.button)
@@ -45,7 +44,49 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
-    fun showText() {
+    override fun onPause() {
+        super.onPause()
+        listener.onStop(this)
+    }
 
+    override fun onResume() {
+        super.onResume()
+        listener.onResume(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        listener.onStart(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        listener.onStop(this)
+    }
+}
+
+class AppLifecycleListener : DefaultLifecycleObserver {
+    var mediaPlayer: MediaPlayer? = null
+
+    fun installMedia(externplayer: MediaPlayer) {
+        mediaPlayer = externplayer
+        mediaPlayer!!.start()
+        mediaPlayer!!.isLooping = true
+        playing = true
+    }
+    private var playing = false
+
+    override fun onStart(owner: LifecycleOwner) { // app moved to foreground
+        if (mediaPlayer != null && !playing) {
+            mediaPlayer!!.start()
+            playing = true
+        }
+    }
+
+    override fun onStop(owner: LifecycleOwner) { // app moved to background
+        if (mediaPlayer != null && playing) {
+            mediaPlayer!!.pause()
+            playing = false
+        }
     }
 }
