a555920bffdd9f76c20f67f5e2b9636eb155d16a
Aaron Glave <aaronglavenowbelivesingod@gmail.com>
2025-08-05 12:25:10 -0500

    Fixed making the button important!!! Mostly at least.

--- app/src/main/java/com/example/jesusdefeatingsatan/MainActivity.kt
+++ app/src/main/java/com/example/jesusdefeatingsatan/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.jesusdefeatingsatan
 
+import android.content.Context
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.widget.Button
@@ -14,30 +16,36 @@ import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import java.util.Timer
 import kotlin.concurrent.schedule
+import androidx.core.view.isInvisible
 
-class MainActivity : AppCompatActivity() {
-    val listener = AppLifecycleListener()
+interface Ender {
+    fun endApp()
+}
+class MainActivity : AppCompatActivity(), Ender {
+    var listener: AppLifecycleListener? = null
+    //val listener = AppLifecycleListener(baseContext, this)
     override fun onCreate(savedInstanceState: Bundle?) {
-        listener.installMedia(MediaPlayer.create(baseContext, R.raw.i_wanna_follow_jesus))
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        /*if (singer != null) {
-            singer!!.start()
-        }*/
-
-        /*singer.setOnCompletionListener {
-            //finish()
-        }
-         */
         setContentView(R.layout.activity_main)
+
         val myMessage = findViewById<TextView>(R.id.aarons_message)
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
-            myMessage.visibility = VISIBLE
-            Timer().schedule(1000) {
-                myMessage.visibility = INVISIBLE
+            if(myMessage.isInvisible) {
+                Log.i("press.button", "Pressed while invisible.")
+                listener?.onUsefulTap()
+                myMessage.visibility = VISIBLE
+                Timer().schedule(1000) {
+                    myMessage.visibility = INVISIBLE
+                }
+            } else {
+                Log.i("press.button", "Pressed while already visible.")
             }
         }
+        listener = AppLifecycleListener(context = baseContext, ender = this)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -46,47 +54,111 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onPause() {
         super.onPause()
-        listener.onStop(this)
+        Log.d("c_time", "pause?")
+        listener?.onPause(this)
     }
 
     override fun onResume() {
         super.onResume()
-        listener.onResume(this)
+        Log.d("c_time", "resume?")
+        listener?.onResume(this)
     }
 
     override fun onStart() {
         super.onStart()
-        listener.onStart(this)
+        Log.d("c_time", "start?")
+        if (listener?.needsStart() == true) {
+            listener?.startPlaying()
+        }
+        listener?.onStart(this)
     }
 
     override fun onStop() {
+        Log.d("c_time", "stop?")
+        listener?.onStop(this)
         super.onStop()
-        listener.onStop(this)
+    }
+
+    override fun endApp() {
+        Log.i("play.song", "Don't wanna listen anymore")
+        finishAndRemoveTask()
     }
 }
 
-class AppLifecycleListener : DefaultLifecycleObserver {
-    var mediaPlayer: MediaPlayer? = null
+class AppLifecycleListener(val context: Context, val ender: Ender) : DefaultLifecycleObserver {
+    private var mediaPlayer: MediaPlayer? = null
+    private var currentPosition: Int = 0
+    private var hasStarted = false
 
-    fun installMedia(externplayer: MediaPlayer) {
-        mediaPlayer = externplayer
-        mediaPlayer!!.start()
-        mediaPlayer!!.isLooping = true
-        playing = true
+    var timesPressed: Int = 0
+
+    fun startPlaying() {
+        mediaPlayer = MediaPlayer.create(context, R.raw.i_wanna_follow_jesus)
+        mediaPlayer?.start()
+        mediaPlayer?.setOnCompletionListener {
+            playSongIfTapped()
+        }
     }
-    private var playing = false
+
+    fun onUsefulTap() {
+        timesPressed += 1
+        Log.d("press.button", "Current count: $timesPressed")
+    }
+
+    fun playSongIfTapped() {
+        if (timesPressed >= 5) {
+            Log.i("c_time", "restarted")
+            timesPressed = 0
+            mediaPlayer = MediaPlayer.create(context, R.raw.i_wanna_follow_jesus)
+            mediaPlayer?.setOnCompletionListener {
+                playSongIfTapped()
+            }
+            mediaPlayer?.start()
+        } else {
+            Log.d("ENDING", "ENDING")
+            ender.endApp()
+        }
+    }
 
     override fun onStart(owner: LifecycleOwner) { // app moved to foreground
-        if (mediaPlayer != null && !playing) {
-            mediaPlayer!!.start()
-            playing = true
+        if (!hasStarted) {
+            if (mediaPlayer != null) {
+                mediaPlayer!!.start()
+            }
+            startPlaying()
+            hasStarted = true
+        } else {
+            if (mediaPlayer != null) {
+                val currentSpot = mediaPlayer?.currentPosition
+                Log.d("c_time","Current spot: ${mediaPlayer?.currentPosition}")
+                if(currentSpot != null) {
+                    Log.d("c_time","Resumed")
+                    mediaPlayer?.seekTo(currentSpot)
+                    mediaPlayer?.start()
+                } else {
+                    Log.d("c_time", "Oh no! Didn't save where we ended...")
+                }
+            }
         }
     }
 
-    override fun onStop(owner: LifecycleOwner) { // app moved to background
-        if (mediaPlayer != null && playing) {
+    override fun onPause(owner: LifecycleOwner) { // app moved to background
+        if (mediaPlayer != null) {
             mediaPlayer!!.pause()
-            playing = false
+            currentPosition = mediaPlayer!!.currentPosition
+            Log.d("c_time", "Current positon: $currentPosition")
         }
     }
+
+    override fun onResume(owner: LifecycleOwner) {
+        super.onResume(owner)
+        if (mediaPlayer != null) {
+            mediaPlayer!!.seekTo(currentPosition)
+        }
+    }
+
+    fun needsStart(): Boolean {
+        return !hasStarted
+    }
+
 }
